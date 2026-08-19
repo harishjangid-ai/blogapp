76c6827d2d12cfa5488c3f74459bed51bf7297d8
harishjangid-ai <hjangid521@gmail.com>
2026-08-19 16:23:33 +0530

    groq error

--- apps/server/server.ts
+++ apps/server/server.ts
@@ -23,7 +23,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://blogapp-web-psi.vercel.app/"],
+    origin: ["http://localhost:3000", "https://blogapp-web-psi.vercel.app"],
     credentials: true,
   }),
 );
